271dc09822d2c0ca7122f83e9e9186cbc17f290a
emmaward1 <eward@s6c.mlp.college>
2026-02-23 11:05:50 +0000

    added full functionality and basic front end

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask import render_template
+import requests
 
 app = Flask(__name__)
+
+PI_URL = "http://192.168.1.42:5000/sensor"
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/sensor")
+def sensor_proxy():
+    try:
+        r = requests.get(PI_URL)
+        return jsonify(r.json())
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
